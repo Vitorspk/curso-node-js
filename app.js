9baa97ceedbd1d2527f98a3dc0e11915201f942a
Vitor Schiavo <vitorschiavo@MacBook-Pro-de-Vitor.local>
2018-01-07 20:13:42 -0200

    Refactoring project to news portal with EJS

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var express = require('express')
 var app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', function(req, res) {
-    res.send("<html><body>Portal de Noticias</body></html>")
+    res.render("home/index")
 })
 
-app.get('/tecnologia', function(req, res) {
-    res.send("<html><body>Noticias de Tecnologia</body></html>")
+app.get('/noticias', function(req, res) {
+    res.render('noticias/noticias')
 })
+
+app.get('/formulario_inclusao_noticia', function(req, res) {
+    res.render('admin/form_add_noticia')
+})
+
 
 app.listen(3000, function() {
     console.log('Servidor rodando com Express')
