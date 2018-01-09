47f2669e0e79ac631b3a7109cdc68f935bb270fe
Vitor Schiavo <vitorschiavo@MacBook-Pro-de-Vitor.local>
2018-01-08 22:27:26 -0200

    retornando valores cadastrados na tabela para a view

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,18 @@
 module.exports = function(app) {
-    app.get('/noticias', function(req, res) {
-        res.render('noticias/noticias')
+
+app.get('/noticias', function(req, res) {
+    var mysql = require('mysql')
+    
+    var connection = mysql.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : 'x5r2i6e3',
+        database : 'portal_noticias'
+    })
+
+    connection.query('select * from noticias', function(erro, result){
+        res.render('noticias/noticias', {noticias : result})
+    })
+    //res.render('noticias/noticias')
     })
 }
