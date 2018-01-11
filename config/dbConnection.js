3ad7c57d0042b2cba1269d1dbb420e05d2899da7
Vitor Schiavo <vitorschiavo@MacBook-Pro-de-Vitor.local>
2018-01-10 22:59:02 -0200

    refatorando porttal de noticias, dbConnection e server

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,12 +1,16 @@
 var mysql = require('mysql')
 
-module.exports = function() {
-
+var connMysql = function() {
     return mysql.createConnection({
         host : 'localhost',
         user : 'root',
         password : 'x5r2i6e3',
         database : 'portal_noticias'
     })
+}
+
+module.exports = function() {
+    console.log('o Auto Load carregou o modulo de conexao com o banco de dados')
+    return connMysql
 }
     
